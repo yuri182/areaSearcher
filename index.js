9bf85c6b5d1a44bd66efab1f18b339972ea2796b
ゆりです。 <116730338+yuri182@users.noreply.github.com>
2022-11-16 18:48:02 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+function getCSV(){
+    var req = new XMLHttpRequest();
+    req.open("get", "rooms.csv", true);
+    req.send(null);
+
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); 
+    }
+}
+ 
+function convertCSVtoArray(str){ 
+    var result = []; 
+    var tmp = str.split("\n"); 
+
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+ 
+}
+ 
+getCSV(); 
