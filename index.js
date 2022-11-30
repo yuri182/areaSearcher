9de6c43de44c3c198771dfb29a7669123e755fe5
ゆりです。 <116730338+yuri182@users.noreply.github.com>
2022-12-01 02:12:35 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -123,10 +123,9 @@ function inputFilter(){    //fillBoxの今の内容を取得
     //さっきの関数の戻り値（allDataに入っている）の特定の値と照合・絞り込み
     let filtered = allData.filter(
         //fillBoxに入力されたもの(inputValue)を0番目の値(ここではname)と大文字小文字関係なく照合させてフィルする
-        ([name, areaSymbol]) => name.includes(inputValue.toUpperCase())
-    ).filter(
-        ([name, areaSymbol]) => radioValue.some((value) => name.includes(value.toUpperCase()))
-    ); // はいなんてこったい同感
+        ([name, areaSymbol]) => name.includes(inputValue.toUpperCase()) && radioValue.some((value) => name.includes(value.toUpperCase()))
+    ); // はいなんてこったい同感こんな深くなるとは思わなかった…JSは悪い文明
+    //うごかないというかまだ完成してないのか
     // すみません、さっき配列を返す関数を作ったので、配列の値の数だけ回る処理をかけようと思います
     //今日こそ何もやってない私(名前変えただけ)←え、関数1個書いた←うーん:confused_face:
     //とりあえず走らせますか...あ、HTMLの方checkedを1個付けないと出だしでエラー起こすかも
